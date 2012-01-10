6f016c915bce019f14f05d99c60090456e4a6961
David C. Bishop <david@davidbishop.org>
2012-01-11 07:36:58 +1100

    VertexArray functions moved out of class decleration and inlined.

--- src/OpenGL/VertexArray.hpp
+++ src/OpenGL/VertexArray.hpp
@@ -8,26 +8,30 @@
 
 class VertexArray {
    public:
-      VertexArray() {
-         glGenVertexArrays(1, &array_);
-         
-      }
-      
-      ~VertexArray() {
-         glDeleteVertexArrays(1, &array_);
-      }
-      
-      void bind() {
-         glBindVertexArray(array_);
-      }
-
-      static void unbind() {
-         glBindVertexArray(0);
-      }
+      VertexArray();      
+      ~VertexArray();
+      void bind();
+      static void unbind();
 
    private:
       GLuint array_;
 };
 typedef std::shared_ptr<VertexArray> VertexArrayPtr;
+
+inline VertexArray::VertexArray() {
+	glGenVertexArrays(1, &array_);
+}
+
+inline VertexArray::~VertexArray() {
+   glDeleteVertexArrays(1, &array_);
+}
+
+inline void VertexArray::bind() {
+   glBindVertexArray(array_);
+}
+
+inline void VertexArray::unbind() {
+   glBindVertexArray(0);
+}
 
 #endif /*  G3TEST_VERTEXARRAY_HPP_ */
